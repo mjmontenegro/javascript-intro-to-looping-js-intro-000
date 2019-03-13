732b068915aaa4b1008c05306a9e87f6e44f127a
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-13 05:00:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 let forLoop = function (arr) {
   let result = [];
-  let for(let i = 0; i < 25; i++) {
+  for(let i = 0; i < 25; i++) {
     forLoop.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
   return result;
