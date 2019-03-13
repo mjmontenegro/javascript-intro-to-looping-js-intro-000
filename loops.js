eabada52a29416f3b636ec08ac8c5df10c822c0d
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-13 05:04:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(arr) {
   let result = [];
   for(let i = 0; i < 25; i++){
-    result.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+    result.push(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`);
   }
   return result;
 }
